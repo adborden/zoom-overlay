110619f2b60effa4d6b78e35b749614e28aae2e4
Aaron D Borden <adborden@a14n.net>
2020-04-02 16:04:16 -0700

    Fix regex

--- update_ebuilds.sh
+++ update_ebuilds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
+set -x
 
 
 function fetch_latest_version () {
 	# HEAD the latest, and get the version from the Location header, removing trailing CRLF.
 	curl --fail --silent -I https://zoom.us/client/latest/zoom_x86_64.tar.xz | \
-		sed -E -n -e '/^Location: / {s!Location: https?://[A-z0-9]+\.cloudfront.net/prod/([0-9.]+)/zoom_x86_64.tar.xz!\1!;p}' | \
+		sed -E -n -e '/^Location: / {s!Location: https?://[A-z0-9]+\.cloudfront.net/prod/([0-9\.]+)/zoom_x86_64.tar.xz.*!\1!;p}' | \
 		tr -d '[:space:]'
 }
 
